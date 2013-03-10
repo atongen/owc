003e9e996399b80d7b3cc248182a5ba66128b97e
Peter Schultz <pleeschultz@gmail.com>
2013-03-09 22:44:42 -0600

    hero carousel updates

--- static/public/scripts/src/hero-carousel.js
+++ static/public/scripts/src/hero-carousel.js
@@ -2,11 +2,18 @@ var HopeApp = (function(app, $){
 
 	var HeroCarousel = function () {
 
+		var autoRotateTimout,
+			autoRotateDelay = 1000,
+			currentSlideIndex = 0,
+			$carousel,
+			$slides,
+			$pagination;
+
 		function init() {
 	
-			var $carousel = $('.hero-carousel');
-			var $slides = $('.slides li', '.hero-carousel');
-			var $pagination = $carousel.find('.pagination');
+			$carousel = $('.hero-carousel');
+			$slides = $('.slides li', '.hero-carousel');
+			$pagination = $carousel.find('.pagination');
 			// build pagination
 			
 			var paginationMarkup = '';
@@ -15,15 +22,40 @@ var HopeApp = (function(app, $){
 				paginationMarkup += '<li><a href="#">•</a></li>';
 			}
 			
-			$carousel.find('.pagination').append(paginationMarkup);
-			
+			$carousel.find('.pagination').append(paginationMarkup);			
 			$pagination.find('li').first().addClass('active');
-			
-			console.log($slides);	
+	
+			beginTimer();
+	
 		}
 	
+		function beginTimer() {
+			autoRotateTimeout = setTimeout(function(){
+				nextSlide();
+			}, autoRotateDelay);
+		}
+		
+		function stopTimer() {
+		
+		}
+	
+		function nextSlide(){
+			var targetSlideIndex = currentSlideIndex + 1;
+			if(targetSlideIndex >= $slides.length){
+				target = 0;
+			}
+			changeSlide(targetSlideIndex);
+		}
+		
+		function previousSlide(){
+			currentSlideIndex
+		}
+		
 		function changeSlide(targetIndex){
 			
+			$currentSlide = $slides.find('.active');
+			console.log('hi', $slides.html());
+			$currentSlide.removeClass('.active');
 			
 			
 		}
