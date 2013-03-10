a6e0ad28e0221ef6e542c8cc4f2fa2f6e0806bb4
Wilson Lewis <wilson.lewis@target.com>
2013-03-10 03:15:17 -0500

    Global Nav for tablet.

--- static/public/scripts/src/app.js
+++ static/public/scripts/src/app.js
@@ -4,20 +4,21 @@ var HopeApp = (function(app, $){
 	 * Initialize Hope App
 	 */
 	function init() {
-
 	    app.heroCarousel.init();
         app.galleryCarousel.init();
         app.easterEgg.init();
+        if ('ontouchstart' in document.documentElement)
+            app.globalNav.init();
 		return app;
 	}
-	
+
 	app.config = {
-	
+
 	};
-	
+
 	app.init = init;
 
-	return app;
+    return app;
 
 })(HopeApp || {}, $);
 
