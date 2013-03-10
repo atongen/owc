350b44002b07bb2a02c2f1fa7b341f7775147bc1
Michael Tighe <striderIIDX@gmail.com>
2013-03-09 22:15:28 -0600

    Add item url resolution to featured items

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,22 @@
 module ApplicationHelper
   include ::FeaturedItemsHelper
 
+  def dynamic_link_for(item)
+    method = item.class.name.underscore.gsub(/refinery\//, '').gsub(/\//, '_')
+    Refinery::Core::Engine.routes.url_helpers.send("#{method}_url".to_s, item, :only_path => true)
+  end
+
   def featured_items
     Refinery::FeaturedItems::FeaturedItem.order(:position).all.map do |item|
       model = namespaced_model(item.model)
       data = model.where(:id => item.model_id).first if model.present?
+
       if data.present?
         {
           :text => item.callout_text,
           :picture => data.picture,
           :title => data.title,
-          :link => "/#{data.class.name.demodulize.underscore.pluralize}/#{data.id}"  #Andrew Tongen did this
+          :link => dynamic_link_for(data)
         }
       end
     end
