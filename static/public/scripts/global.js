3ca7deddfc378c465b41e9b22adcc54de7e525d4
Peter Schultz <pleeschultz@gmail.com>
2013-03-09 19:53:01 -0600

    merging

--- static/public/scripts/global.js
+++ static/public/scripts/global.js
@@ -17,7 +17,6 @@ var HopeApp = (function(app, $){
 	    
 	    console.log(app.heroCarousel);
 	    app.heroCarousel.init();
-        app.galleryCarousel.init();
 		return app;
 	}
 	
@@ -47,8 +46,8 @@ var HopeApp = (function(app, $){
 	var HeroCarousel = function () {
 
 		function init() {
-
-			console.log('Carousel');
+	
+			console.log('Carousel');	
 			return app;
 		}
 	
@@ -61,71 +60,4 @@ var HopeApp = (function(app, $){
 	
 	return app;
 
-})(HopeApp || {}, $);
-
-/* **********************************************
-     Begin gallery-carousel.js
-********************************************** */
-
-var HopeApp = (function(app, $){
-    var GalleryCarousel = function () {
-        function init() {
-            console.log('Gallery');
-            $(".gallery-carousel").each(function(index, view) {
-                new app.galleryCarousel.Gallery($(view));
-            });
-            return app;
-        }
-
-        function Gallery(view) {
-            var self = this;
-            self.width = view.width();
-            self.imageContainer = view.find(".gallery-carousel-image-container")
-            self.images = self.imageContainer.find(".gallery-carousel-image");
-            self.index = 0;
-
-            self.gotoIndex = function(index, animated, direction) {
-                if (index < 0)
-                    index += self.images.length;
-                if (index > self.images.length - 1)
-                    index -= self.images.length;
-
-                self.index = index;
-
-                var offset = 0;
-                if (direction)
-                    offset = direction === "next" ? self.width : -self.width;
-
-                $(self.images[self.index]).stop(true, true)
-                    .appendTo(self.imageContainer)
-                    .css("left", offset)
-                    .animate({left: 0}, !animated ? 0 : "normal");
-            }
-
-            self.gotoNextImage = function(animated) {
-                self.gotoIndex(self.index+1, true, "next");
-            }
-
-            self.gotoPreviousImage = function(animated) {
-                self.gotoIndex(self.index-1, true, "previous");
-            }
-
-            view.find(".gallery-carouse-navigation-previous").click(function(event) {
-                self.gotoPreviousImage(true);
-            });
-
-            view.find(".gallery-carouse-navigation-next").click(function(event) {
-                self.gotoNextImage(true);
-            });
-
-            self.gotoIndex(0, false);
-        }
-
-        return {
-            init: init,
-            Gallery: Gallery
-        }
-    }
-    app.galleryCarousel = new GalleryCarousel();
-    return app;
 })(HopeApp || {}, $);
