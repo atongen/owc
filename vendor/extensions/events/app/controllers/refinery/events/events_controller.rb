f79fef7f40045016f54f1df24180538f13709ee2
Andrew Tongen <atongen@gmail.com>
2013-03-23 12:28:42 -0500

    Simplify events templating

--- vendor/extensions/events/app/controllers/refinery/events/events_controller.rb
+++ vendor/extensions/events/app/controllers/refinery/events/events_controller.rb
@@ -2,7 +2,6 @@ module Refinery
   module Events
     class EventsController < ::ApplicationController
 
-      # upcoming events
       def index
         @events = Event.order('position ASC')
         # you can use meta fields from your model instead (e.g. browser_title)
@@ -15,12 +14,14 @@ module Refinery
         @events = Event.where("date(start_date) >= ?", Date.today)
         @page = ::Refinery::Page.where(:link_url => "/events/upcoming").first
         present(@page)
+        render :action => :index
       end
 
       def past
         @events = Event.where("date(start_date) < ?", Date.today)
         @page = ::Refinery::Page.where(:link_url => "/events/past").first
         present(@page)
+        render :action => :index
       end
 
     end
